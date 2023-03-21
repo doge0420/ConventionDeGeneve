4c136188531d63df24692b0bfba18e04b35b7515
Doge0420 <ethansoya@gmail.com>
2023-03-21 09:50:39 +0100

    oui

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
-class jeu(self,difficuty,v1orBot):
+class jeu:
+    def __init__(self):
+        pass
+
+    def word_difficulty(self, word):
+        lenght = len(word)
 
 
-        
+# https://scriptingosx.com/2020/03/macos-shell-command-to-create-a-new-terminal-window/
