0b5e66653f67c4c9d16207d2ffe0bf33655ade9a
swaggbln <nadav.solomon@bluewin.ch>
2023-03-23 14:53:38 +0100

    menu test, erreur non identifiée dans init

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ class GUI:
 
     def __init__(self):
         with open("title.txt", "r", encoding="utf-8") as f:
-            print(f.read())
+            print(f.read() + "\n1. PLAY \n2. GAME MODE \n3. HOW TO PLAY")
         
 ok = GUI()
 
