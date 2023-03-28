f21272048b15b0aebc8cc9ab8f3517ce1e88d081
swaggbln <nadav.solomon@bluewin.ch>
2023-03-28 09:30:03 +0200

    gui update + retourne les valeurs

--- gui.py
+++ gui.py
@@ -6,50 +6,54 @@ class GUI:
         with open("floppatest.txt", "r", encoding="utf-8") as g:
             print(g.read()) #print big floppa en ascii, on peut remplacer plus tard par mrincredible
 
-        choix = input("le choix")
+        self.players, self.difficulty = self.menu2()
     
+
+
+
+    def menu2(self):
+        choix = input("le choix")
         if choix == "1":
             print("le jeu gaming")
+            
+            return None,None
 
         elif choix == "2":
-            print("1.NOMBRE DE JOUEURS ([1])([2]) \n2.DIFFICULTE ([1])([2])([3]) \nMODE INCREDIBLE ([n])([y])")
+            print("1.NOMBRE DE JOUEURS ([1])([2]) \n2.DIFFICULTE ([1])([2])([3]) \n3.MODE INCREDIBLE ([n])([y])")
 
-            choix_gm = input("(1/2/3)")
+            gamemode = input("(1/2/3)")
 
             bruh = "#dilexyies"
             players = 1
             difficulty = 2
             incredible_mode = "n"
 
-            if choix_gm == "1":
+            if gamemode == "1":
                 n_players = input("nombre de joueurs (1/2)?")
 
-            if n_players == "1" or "2":
-                players = n_players
-            else:
-                players = players
-                print(bruh)
+                if n_players == "1" or "2":
+                    players = n_players
+                else:
+                    print(bruh)
+                    
+                return players, None
 
-            if choix_gm == "2":
+            if gamemode == "2":
                 n_difficulty = input("difficulté des mots? (1/2/3)")
                 if n_difficulty == "1" or "2" or "3":
                     difficulty = n_difficulty
                 else:
-                    difficulty = difficulty
                     print(bruh)
-                
 
-
-
-
-
-
-
+            return None, difficulty
 
         elif choix == "3":
             print("la honte il sait pas comment jouer")
-        else:
-            print("soulja boy est le premier rappeur avec un iphone")
+            return None,None
+        #else:
+            #print("soulja boy est le premier rappeur avec un iphone")
+        
+            
 
     
 
