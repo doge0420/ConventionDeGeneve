8040fb41c3344ca87e91c7684d9b7ccb7d48cd31
Doge0420 <ethansoya@gmail.com>
2023-03-30 15:24:16 +0200

    gouillie

--- gui.py
+++ gui.py
@@ -1,56 +1,85 @@
 class GUI:
 
     def __init__(self):
+        self.choice = {"joueurs" : 1, "difficulte" : 1, "mode_incredible" : False, "pseudo" : None} # pour le choix du joueur -> list[n_settings, parametres (si aucun => None)]
+        self.flag = True
+
+        # ascii
         self.load_ascii()
-        # appeler les fonctions
+
+        # ask pseudo
+        self.ask_pseudo()
+
+        # loop du choix
+        self.choice_loop()
+
+    # debug
+    def __del__(self):
+        print(self.choice)
+
+    def ask_pseudo(self):
+        pseudo = input("Quelle est votre pseudo ? : ")
+        self.choice["pseudo"] = pseudo
 
     def load_ascii(self):
         with open("title.txt", "r", encoding="utf-8") as f:
             print(f.read())
-        with open("floppatest.txt", "r", encoding="utf-8") as g:
-            print(g.read()) #print big floppa en ascii, on peut remplacer plus tard par mrincredible
+        # with open("floppatest.txt", "r", encoding="utf-8") as g:
+        #     print(g.read()) #print big floppa en ascii, on peut remplacer plus tard par mrincredible
+
+    def choice_loop(self):
+        while self.flag:
+            self.get_choice()
 
     def get_choice(self):
-        choice = input("1. PLAY \n2. GAME MODE \n3. HOW TO PLAY\nVotre choix : ")
+        choice = input("1. PLAY \n2. SETTINGS \n3. HOW TO PLAY\nVotre choix : ")
 
         if choice == "1":
-            print("le jeu gaming")
-            
-            return None,None
+            ... # lance le jeu
+            self.flag = False
 
         elif choice == "2":
-            self.get_gamemode()
+            self.get_settings()
 
         elif choice == "3":
-            print("la honte il sait pas comment jouer")
+            ... # print un document qui aide
+        
+        else:
+            print("\nNumero invalide\n")
 
-        return None,None
+    def get_settings(self):
+        setting = input("1.NOMBRE DE JOUEURS ([1])([2]) \n2.DIFFICULTE ([1])([2])([3]) \n3.MODE INCREDIBLE ([n])([y])\n4.ARRIERE\nChoix du reglage : ")
 
-    # list[n_gamemode, parametre]
-    def get_gamemode(self):
-        gamemode = input("1.NOMBRE DE JOUEURS ([1])([2]) \n2.DIFFICULTE ([1])([2])([3]) \n3.MODE INCREDIBLE ([n])([y])\nChoix du gamemode : ")
-
-        if gamemode == "1":
+        if setting == "1":
             n_players = input("Nombre de joueurs (1/2)? : ")
 
             if n_players == "1" or "2":
-                players = n_players
+                self.choice["joueurs"] = n_players
             else:
                 print("#dilexyies")
-                
-            return [1, players]
 
-        if gamemode == "2":
+        elif setting == "2":
             n_difficulty = input("difficulté des mots (1/2/3)? : ")
             if n_difficulty == "1" or "2" or "3":
-                difficulty = n_difficulty
+                self.choice["difficulte"] = n_difficulty
             else:
                 print("#dilexyies")
 
-            return [2, difficulty]
+        elif setting == "3":
+            choice = input("(y/n) ? : ")
+            
+            if choice == "y":
+                self.choice["mode_incredible"] = True
+            elif choice == "n":
+                self.choice["mode_incredible"] = False
+            else:
+                print("#dilexyies")
 
-        if gamemode == "3":
-            return [3, None]
+        elif setting == "4":
+            self.get_choice()
+
+        else:
+            print("\nNumero invalide\n")
 
 if __name__ == "__main__": 
     ok = GUI()
