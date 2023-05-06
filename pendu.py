87299a0ed19c9b56bba57825079c457509f2e33b
Doge0420 <ethansoya@gmail.com>
2023-05-06 16:26:08 +0200

    word class

--- pendu.py
+++ pendu.py
@@ -5,7 +5,7 @@ from time import sleep
 from PIL import Image
 import json
 
-class GAME(GUI):
+class PENDU(GUI):
     def __init__(self):
         super().__init__()
 
@@ -13,36 +13,15 @@ class GAME(GUI):
         self.run = True
         self.guesses = []
         self.bad_guess = []
+        
+        self.word = WORD()
 
         self.menu()
 
         self.game_loop()
 
-    def _choose_word(self):
-        with open("ressources/words.json", "r") as f:
-            words = json.load(f)
-
-        word_range = words[self.choice["difficulte"]]
-
-        word_choice = choice(list(word_range.items()))
-
-        print(word_choice)
-
-        return [i for i in word_choice[0]], word_choice[1]
-        
-    # plus besoin
-    # def _word_difficulty(self, word):
-    #     lenght = len(word)
-
-    #     bonus = 0
-    #     for letter in word:
-    #         if letter in self.LEAST_USED_LETTERS:
-    #             bonus += 2
-
-    #     return lenght + bonus
-
     def game_loop(self):
-        word_list, word_diff = self._choose_word()
+        word_list, word_diff = self.word.get_word(self.choice["difficulty"])
 
         self.clear_screen()
         self.display_word_guess(word_list, self.guesses, self.bad_guess, word_diff)
@@ -62,7 +41,7 @@ class GAME(GUI):
                     print("Bonne lettre :)")
 
                     # win
-                    if self._check_win(word_list) and (len(set(word_list)) == len(set(self.guesses))):  
+                    if self.word.check_win(self.guesses) and (len(set(word_list)) == len(set(self.guesses))):  
                         self.clear_screen()
                         self.run = False
                         
@@ -94,9 +73,6 @@ class GAME(GUI):
                 self.display_word_guess(word_list, self.guesses, self.bad_guess, word_diff)
                 guess_input = input("Veuillez choisir une lettre : ")
 
-    def _check_win(self, word_list):
-        return all(i in self.guesses for i in word_list)
-
     def _mode_incredible(self, win=False):
         if win:
             with Image.open("ressources/incredible/mincredible_bravo.png") as f:
@@ -110,5 +86,31 @@ class GAME(GUI):
         with open (f"ressources/pendus11/pendu{len(self.bad_guess)}.txt", "r", encoding="utf-8") as f:
             print(f.read())
 
+class WORD:
+    def get_word(self, difficulte):
+        with open("ressources/words.json", "r") as f:
+            words = json.load(f)
+
+        word_range = words[difficulte]
+        word_choice = choice(list(word_range.items()))
+
+        self.word_list = [i for i in word_choice[0]]
+
+        return self.word_list, word_choice[1]
+    
+    # plus besoin
+    def word_difficulty(self, word):
+        lenght = len(word)
+
+        bonus = 0
+        for letter in word:
+            if letter in self.LEAST_USED_LETTERS:
+                bonus += 2
+
+        return lenght + bonus
+
+    def check_win(self, guesses):
+        return all(i in guesses for i in self.word_list)
+
 if __name__ == "__main__":
-    GAME()
+    PENDU()
